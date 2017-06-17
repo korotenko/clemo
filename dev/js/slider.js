eb629b26bba5a7c049c8c8e2022f58ec24509043
Korotenko Victoria <korotenkovika6@gmail.com>
2017-06-17 20:16:39 +0300

    убрала лишнее

--- dev/js/slider.js
+++ dev/js/slider.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
-    $("#carousel-best-team").carousel({
-        interval: !1
-    }), $("ul.indicators>li").click(function() {
+    $("ul.indicators>li").click(function() {
         $(this).parent().hasClass("with-counter") && $("p.with-counter").text($(this).data("slide-to") + 1 + "/5"), $(this).prevAll().removeClass("selected"), $(this).nextAll().removeClass("selected"), $(this).addClass("selected")
     }), $("span.arrow-right").click(function() {
         var e = $("#carousel-best-team div.carousel-inner").find("div.item.active").index() + 1;
